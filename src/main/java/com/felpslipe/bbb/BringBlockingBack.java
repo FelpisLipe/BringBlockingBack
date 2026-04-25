adae5c578d931597c6335d5a72c099d532c509b7
FelpisLipe <176348835+FelpisLipe@users.noreply.github.com>
2026-04-24 22:16:38 -0300

    Config system

--- src/main/java/com/felpslipe/bbb/BringBlockingBack.java
+++ src/main/java/com/felpslipe/bbb/BringBlockingBack.java
@@ -1,5 +1,6 @@
 package com.felpslipe.bbb;
 
+import com.felpslipe.bbb.config.ConfigManager;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.Minecraft;
 import org.slf4j.Logger;
@@ -8,11 +9,11 @@ import org.slf4j.LoggerFactory;
 public class BringBlockingBack implements ClientModInitializer {
 	public static final String MOD_ID = "bring-blocking-back";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	public static Minecraft client;
 	@Override
 	public void onInitializeClient() {
 		client = Minecraft.getInstance();
 		LOGGER.info("[BBB] Bringing blocking back..");
+		ConfigManager.load();
 	}
 }
---
+++ src/main/java/com/felpslipe/bbb/config/Config.java
@@ -0,0 +1,8 @@
+package com.felpslipe.bbb.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Config {
+    public List<String> blockables = new ArrayList<>(List.of("#minecraft:swords"));
+}
